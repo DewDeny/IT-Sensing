4814efb55894b7fa0301a6f397efa09b19cd5f9c
Stefanus Deny <maxoxygenless@gmail.com>
2025-11-26 11:12:51 +0700

    unfinished zoom system
    adding zoom function.

--- Assets/Scripts/ViewControl.cs
+++ Assets/Scripts/ViewControl.cs
@@ -6,11 +6,13 @@ public class ViewControl : MonoBehaviour
 {
     Vector2 mouseInitPos, mousePos;
     Vector3 cameraInitPos, cameraInitRot;
+  GameObject cameraObj;
 
     // Start is called before the first frame update
     void Start()
     {
         cameraInitPos = transform.localPosition;
+        cameraObj = transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
@@ -29,14 +31,14 @@ public class ViewControl : MonoBehaviour
 
             Quaternion thisRotation = Quaternion.identity;
             thisRotation.eulerAngles = new Vector3(
-            cameraInitRot.x - (mousePos.y - mouseInitPos.y)/10,
-            cameraInitRot.y + (mousePos.x - mouseInitPos.x)/10,
+            cameraInitRot.x - (mousePos.y - mouseInitPos.y) / 10,
+            cameraInitRot.y + (mousePos.x - mouseInitPos.x) / 10,
             cameraInitRot.z);
 
             transform.localEulerAngles = thisRotation.eulerAngles;
         }
 
-                //Strafing control
+        //Strafing control
         if (Input.GetMouseButtonDown(1))
             mouseInitPos = Input.mousePosition;
 
@@ -47,10 +49,16 @@ public class ViewControl : MonoBehaviour
         {
             mousePos = Input.mousePosition;
 
-            transform.localPosition = new Vector3(
-                cameraInitPos.x - (mousePos.x - mouseInitPos.x) / 10,
-                cameraInitPos.y - (mousePos.y - mouseInitPos.y) / 10,
-                cameraInitPos.z);
+            transform.position = cameraInitPos
+                - (transform.right * (mousePos.x - mouseInitPos.x) / 8)
+                - (transform.up * (mousePos.y - mouseInitPos.y) / 8);
         }
+
+        //Zooming control
+        cameraObj.transform.position = new Vector3(cameraObj.transform.position.x,
+            cameraObj.transform.position.y,
+            cameraObj.transform.position.z * Input.mouseScrollDelta.x);
+
+        Debug.Log(Input.mouseScrollDelta);
     }
 }
